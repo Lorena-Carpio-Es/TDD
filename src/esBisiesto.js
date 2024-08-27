a7d2dc92700dca7ce14e08cf60db7b073abc07b4
Lorena-Carpio-Es <karylore1@gmail.com>
2024-08-27 07:10:33 -0400

    Anios divisibles por 400 son anios bisiestos

--- src/esBisiesto.js
+++ src/esBisiesto.js
@@ -1,8 +1,14 @@
-function esBisiesto(anio){
-    if (anio === 4){
+function esBisiesto(anio) {
+    if (anio % 400 === 0) {
         return true;
+    } else if (anio % 100 === 0) {
+        return false;
+    } else if (anio % 4 === 0) {
+        return true;
+    } else {
+        return false;
     }
-    return false;
 }
+
 
 export default esBisiesto;
