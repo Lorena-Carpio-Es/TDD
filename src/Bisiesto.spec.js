5ba77e44589e5818973af0dfc12d47a1b15de2aa
Lorena-Carpio-Es <karylore1@gmail.com>
2024-08-27 07:17:52 -0400

    Anios divisibles por 4 pero no por 100 son anios bisiestos

--- src/Bisiesto.spec.js
+++ src/Bisiesto.spec.js
@@ -28,6 +28,18 @@ describe("Es Bisiesto", () => {
     it("El año 2100 no debería considerarse como año bisiesto", () => {
         expect(esBisiesto(2100)).toEqual(false);
     });
+
+    it("El año 2008 debería considerarse como año bisiesto", () => {
+        expect(esBisiesto(2008)).toEqual(true);
+    });
+
+    it("El año 2012 debería considerarse como año bisiesto", () => {
+        expect(esBisiesto(2012)).toEqual(true);
+    });
+
+    it("El año 2016 debería considerarse como año bisiesto", () => {
+        expect(esBisiesto(2016)).toEqual(true);
+    });
 });
 
   
